9bf2e8c6c1c0ebcc04a47d2d46a06bc912f3e0c7
Lorenzo Hucke <lorenzohucke@gmail.com>
2025-09-17 16:06:29 -0300

    añadido feature branch

--- src/component/Catalog.tsx
+++ src/component/Catalog.tsx
@@ -4,7 +4,7 @@ import { Link } from "react-router-dom";
 const mockProducts = [
   { id: 1, title: "Tour en la ciudad", place: "Santiago", price: 50, duration: "2h", rating: 4.5 },
   { id: 2, title: "Excursión montaña", place: "Andes", price: 100, duration: "6h", rating: 4.8 },
-  { id: 3, title: "Visita museo", place: "Valparaíso", price: 30, duration: "1h", rating: 4.2 },
+  { id: 3, title: "trekking", place: "Cochamó", price: 30, duration: "1h", rating: 4.2 },
   { id: 4, title: "Clases de surf", place: "Viña del Mar", price: 80, duration: "3h", rating: 4.6 },
 ];
 
